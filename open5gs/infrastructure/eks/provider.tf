23048b34dd1063e8ccec8593e8ea4cfae62c139b
Alexander Wise <contact@alexwise.guru>
2023-06-01 18:58:24 +0000

    Update file provider.tf

--- open5gs/infrastructure/eks/provider.tf
+++ open5gs/infrastructure/eks/provider.tf
@@ -8,20 +8,20 @@ data "aws_eks_cluster" "default" {
 
 provider "kubernetes" {
   host                   = module.eks.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.eks_cluster_id]
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
   }
 }
 
 provider "kubectl" {
   load_config_file       = false
   host                   = module.eks.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
@@ -32,7 +32,7 @@ provider "kubectl" {
 provider "helm" {
   kubernetes {
     host                   = module.eks.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
