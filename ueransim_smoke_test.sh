27fc180940566b9d6d723329f038707e613787d0
Alexander Wise <contact@alexwise.guru>
2023-06-09 22:37:17 +0000

    Update 3 files
    - /curl-format.txt
    - /ueransim_smoke_test.sh
    - /.gitlab-ci.yml

--- ueransim_smoke_test.sh
+++ ueransim_smoke_test.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-echo ip addr
-ping -I uesimtun0 -c 1 google.com 
-# traceroute -i uesimtun0 google.com
-curl --interface uesimtun0 https://www.google.com
+curl -I ueransim0 -w "@curl-format.txt" -o over5g.txt -s --url https://google.com
 
-if  [ $? -eq 0 ]
-then
-   echo "successful test"
-   exit
-else
-   echo "test failed" 
-fi
+curl -I eth0 google.com -w "@curl-format.txt" -o overinternet.txt -s --url https://google.com
