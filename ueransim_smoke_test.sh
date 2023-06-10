706e43f0c10c3c0572d706dbedc1461e1910cd18
Alexander Wise <contact@alexwise.guru>
2023-06-10 03:20:12 +0000

    Update file ueransim_smoke_test.sh

--- ueransim_smoke_test.sh
+++ ueransim_smoke_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -I ueransim0 -w "@curl-format.txt" -o over5g.txt -s --url https://google.com
+curl -I ueransim0 -w "@/tmp/curl-format.txt" -o over5g.txt -s --url https://google.com
 
-curl -I eth0 google.com -w "@curl-format.txt" -o overinternet.txt -s --url https://google.com
+curl -I eth0 google.com -w "@/tmp/curl-format.txt" -o overinternet.txt -s --url https://google.com
