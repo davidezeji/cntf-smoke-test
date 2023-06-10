43eb10a4d4cacbeb3ee0e9bffa3a65f8ccae06ad
Alexander Wise <contact@alexwise.guru>
2023-06-10 03:47:29 +0000

    Update 3 files
    - /curl-format.txt
    - /ueransim_smoke_test.sh
    - /.gitlab-ci.yml

--- ueransim_smoke_test.sh
+++ ueransim_smoke_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-curl -I ueransim0 -w "@/tmp/curl-format.txt" -o over5g.txt -s --url https://google.com
 
-curl -I eth0 google.com -w "@/tmp/curl-format.txt" -o overinternet.txt -s --url https://google.com
+
+curl -I ueransim0 -w %{json} --url https://yahoo.com --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee over5g.json
+
+curl -I eth0 -w %{json} --url https://yahoo.com --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee overinternet.json
