00aa69a41843e47644e5b7ea69e3e1afe7ad75c3
Alexander Wise <contact@alexwise.guru>
2023-06-10 04:05:18 +0000

    Update 2 files
    - /ueransim_smoke_test.sh
    - /.gitlab-ci.yml

--- ueransim_smoke_test.sh
+++ ueransim_smoke_test.sh
@@ -3,6 +3,6 @@ set -x
 
 apk add --no-cache jq
 
-curl -I ueransim0 -w %{json} --url https://yahoo.com --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee over5g.json
+curl -i ueransim0 -w %{json} --url https://yahoo.com --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee over5g.json
 
-curl -I eth0 -w %{json} --url https://yahoo.com --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee overinternet.json
+curl -i eth0 -w %{json} --url https://yahoo.com --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee overinternet.json
