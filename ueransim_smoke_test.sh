023516825f58921f763d885ec25774964841f130
Alexander Wise <contact@alexwise.guru>
2023-06-10 04:25:17 +0000

    Update file ueransim_smoke_test.sh

--- ueransim_smoke_test.sh
+++ ueransim_smoke_test.sh
@@ -3,6 +3,6 @@ set -x
 
 apk add --no-cache jq
 
-curl --interface uesimtun0 -w %{json} --url https://yahoo.com --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee over5g.json
+curl --interface uesimtun0 -w %{json} --url https://www.cia.gov/the-world-factbook/about/archives/download/factbook-2020.zip?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee over5g.json
 
-curl --interface eth0 -w %{json} --url https://yahoo.com --silent --output /dev/null | jq -c '. + {"test": "overinternet"}' | tee overinternet.json
+curl --interface eth0 -w %{json} --url https://www.cia.gov/the-world-factbook/about/archives/download/factbook-2020.zip?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "overinternet"}' | tee overinternet.json
