9129be9338838b4cd1d568d4af7a61f88ef7da37
Alexander Wise <contact@alexwise.guru>
2023-06-10 04:12:26 +0000

    Update file ueransim_smoke_test.sh

--- ueransim_smoke_test.sh
+++ ueransim_smoke_test.sh
@@ -3,6 +3,6 @@ set -x
 
 apk add --no-cache jq
 
-curl -i ueransim0 -w %{json} --url https://yahoo.com --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee over5g.json
+curl --interface uesimtun0 -w %{json} --url https://yahoo.com --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee over5g.json
 
-curl -i eth0 -w %{json} --url https://yahoo.com --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee overinternet.json
+curl --interface eth0 -w %{json} --url https://yahoo.com --silent --output /dev/null | jq -c '. + {"test": "overinternet"}' | tee overinternet.json
