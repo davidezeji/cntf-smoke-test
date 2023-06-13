1c83e7c571712712425b9a3be1cb410c965e5f89
Alexander Wise <contact@alexwise.guru>
2023-06-13 04:43:56 +0000

    Update 2 files
    - /ueransim_smoke_test.sh
    - /.gitlab-ci.yml

--- ueransim_smoke_test.sh
+++ ueransim_smoke_test.sh
@@ -3,6 +3,8 @@ set -x
 
 apk add --no-cache jq
 
-curl --interface uesimtun0 -w %{json} --url https://www.cia.gov/the-world-factbook/about/archives/download/factbook-2020.zip?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "over5g"}' >> /proc/1/fd/1 | tee over5g.json
+curl --interface uesimtun0 -w %{json} --url https://yahoo.com?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee over5g.json
 
-curl --interface eth0 -w %{json} --url https://www.cia.gov/the-world-factbook/about/archives/download/factbook-2020.zip?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "overinternet"}' >> /proc/1/fd/1 | tee overinternet.json
+curl --interface eth0 -w %{json} --url https://yahoo.com?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "overinternet"}' | tee overinternet.json
+
+# https://www.cia.gov/the-world-factbook/about/archives/download/factbook-2020.zip?$RANDOM
