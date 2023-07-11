d7a435cd53c7866ba1238288ad7cfd8583a9d2f4
davidezeji <ezejidavid@gmail.com>
2023-07-11 15:13:52 -0500

    added python script to upload local test files to s3 and created bash script to reflect new changes to s3 objects every 5 minutes

---
+++ update_s3_test_results.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+aws s3 cp ~/cntf/over5g.json s3://cntf-open5gs-coralogix-test-results/over5g.json
+aws s3 cp ~/cntf/overinternet.json s3://cntf-open5gs-coralogix-test-results/overinternet.json
