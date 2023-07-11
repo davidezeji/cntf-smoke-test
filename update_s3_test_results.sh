d2ef00500f3c17777d51487cc61c606e1a18be68
davidezeji <ezejidavid@gmail.com>
2023-07-11 16:08:27 -0500

    added comment to update_s3_test_results.sh file

--- update_s3_test_results.sh
+++ update_s3_test_results.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# this file updates the objects in the s3 bucket "cntf-open5gs-coralogix-test-results" with any changes made to the local files: "over5g.json" or "overinternet.json" 
+
 aws s3 cp ~/cntf/over5g.json s3://cntf-open5gs-coralogix-test-results/over5g.json
 aws s3 cp ~/cntf/overinternet.json s3://cntf-open5gs-coralogix-test-results/overinternet.json
