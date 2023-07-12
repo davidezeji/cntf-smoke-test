81b94f59b316519d59bb04a225c9aaa7b726042d
davidezeji <ezejidavid@gmail.com>
2023-07-12 14:52:31 -0500

    modified python script to add a condition which does not upload s3 objects if they already exist.

--- s3_test_results_coralogix.py
+++ s3_test_results_coralogix.py
@@ -2,11 +2,39 @@
 
 # this script uploads the local test result files (e.g. over5g.json & overinternet.json) as objects in an AWS S3 bucket.
 
+# import os
+# import pathlib
+
+# from glob import glob
+# import boto3
+
+# BASE_DIR = pathlib.Path(__file__).parent.resolve()
+# FILE_DIR = os.path.join(BASE_DIR)
+
+# BUCKET_NAME = 'cntf-open5gs-coralogix-test-results'
+# AWS_REGION = 'us-east-1'
+
+# def upload_file(file_name, bucket, object_name):
+#     s3_resource = boto3.resource('s3', region_name=AWS_REGION)
+
+#     if object_name is None:
+#         object_name = file_name
+    
+#     s3_resource.meta.client.upload_file(file_name, BUCKET_NAME, object_name)
+
+# if __name__ == '__main__':
+#     file_name = os.path.join(FILE_DIR, 'over5g.json')
+#     upload_file(file_name=file_name, bucket=BUCKET_NAME, object_name='over5g.json')
+
+# if __name__ == '__main__':
+#     file_name = os.path.join(FILE_DIR, 'overinternet.json')
+#     upload_file(file_name=file_name, bucket=BUCKET_NAME, object_name='overinternet.json')
+
 import os
 import pathlib
-
 from glob import glob
 import boto3
+import botocore.exceptions
 
 BASE_DIR = pathlib.Path(__file__).parent.resolve()
 FILE_DIR = os.path.join(BASE_DIR)
@@ -19,8 +47,21 @@ def upload_file(file_name, bucket, object_name):
 
     if object_name is None:
         object_name = file_name
-    
-    s3_resource.meta.client.upload_file(file_name, BUCKET_NAME, object_name)
+
+    s3_client = s3_resource.meta.client
+
+    # Check if the object already exists in the bucket
+    try:
+        s3_client.head_object(Bucket=bucket, Key=object_name)
+        print(f"Object '{object_name}' already exists in bucket '{bucket}'. Skipping upload.")
+        return
+    except botocore.exceptions.ClientError as e:
+        # Object does not exist, proceed with upload
+        if e.response['Error']['Code'] == '404':
+            s3_client.upload_file(file_name, bucket, object_name)
+        else:
+            # Other error occurred
+            raise
 
 if __name__ == '__main__':
     file_name = os.path.join(FILE_DIR, 'over5g.json')
