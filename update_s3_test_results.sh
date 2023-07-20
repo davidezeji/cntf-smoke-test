f64bebb2468e61daf64cb8947822e44dbf34fd07
David Ezeji <ezejidavid@gmail.com>
2023-07-20 17:17:05 +0000

    Update update_s3_test_results.sh

--- update_s3_test_results.sh
+++ update_s3_test_results.sh
@@ -2,6 +2,9 @@
 
 # this script updates the objects in the s3 bucket "cntf-open5gs-coralogix-test-results" with any changes made to their corresponding local files. 
 
-aws s3 cp ./over5g.json s3://cntf-open5gs-coralogix-test-results/over5g.json
-aws s3 cp ./overinternet.json s3://cntf-open5gs-coralogix-test-results/overinternet.json
+udpate_s3() {
+   aws s3 cp ./over5g.json s3://cntf-open5gs-coralogix-test-results/over5g.json
+   aws s3 cp ./overinternet.json s3://cntf-open5gs-coralogix-test-results/overinternet.json
+}
 
+udpate_s3
