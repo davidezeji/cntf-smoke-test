5f72f31f85723a11ae81a0b4c32229deceaa969b
davidezeji <ezejidavid@gmail.com>
2023-08-02 14:47:43 -0500

    added the correct s3 bucket to script

--- update_s3_test_results.sh
+++ update_s3_test_results.sh
@@ -3,8 +3,8 @@
 # this script updates the objects in the s3 bucket "cntf-open5gs-coralogix-test-results" with any changes made to their corresponding local files. 
 
 udpate_s3() {
-   aws s3 cp ./over5g.json s3://cntf-open5gs-coralogix-test-results/over5g.json
-   aws s3 cp ./overinternet.json s3://cntf-open5gs-coralogix-test-results/overinternet.json
+   aws s3 cp ./over5g.json s3://cntf-open5gs-test-results/over5g.json
+   aws s3 cp ./overinternet.json s3://ccntf-open5gs-test-results/overinternet.json
 }
 
 udpate_s3
