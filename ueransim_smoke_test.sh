03fa8cf4c8cd5283326662b8195819abaec4b864
Pierce A <pbalworth@gmail.com>
2023-08-28 22:40:18 +0000

    Update to 200 curls ueransim_smoke_test.sh

--- ueransim_smoke_test.sh
+++ ueransim_smoke_test.sh
@@ -3,8 +3,16 @@ set -x
 
 apk add --no-cache jq
 
-curl --interface uesimtun0 -w %{json} --url https://yahoo.com?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee over5g.json
+for i in {1..200}; do
+    # Run the first curl command
+    curl --interface uesimtun0 -w %{json} --url https://yahoo.com?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee -a over5g.json
 
-curl --interface eth0 -w %{json} --url https://yahoo.com?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "overinternet"}' | tee overinternet.json
+    # Run the second curl command
+    curl --interface eth0 -w %{json} --url https://yahoo.com?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "overinternet"}' | tee -a overinternet.json
+done
+
+# curl --interface uesimtun0 -w %{json} --url https://yahoo.com?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee over5g.json
+
+# curl --interface eth0 -w %{json} --url https://yahoo.com?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "overinternet"}' | tee overinternet.json
 
 # https://www.cia.gov/the-world-factbook/about/archives/download/factbook-2020.zip?$RANDOM
