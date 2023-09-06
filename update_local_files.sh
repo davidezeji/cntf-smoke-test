7a4505ef7bf7c04302a30d25564ac69f985a73f4
David Ezeji <ezejidavid@gmail.com>
2023-09-06 16:08:11 +0000

    Delete update_local_files.sh

--- update_local_files.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-POPULATE_POD=$(kubectl -n openverso get pod --output=jsonpath={.items..metadata.name} -l app.kubernetes.io/component=populate)
-
-update_local_files () {
-    kubectl cp -n openverso $POPULATE_POD:/time_to_populate_database.txt ./time_to_populate_database.txt
-}
-
-update_local_files
