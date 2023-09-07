fb327cbd7c147c63655b85c0cd66aded72deabc3
Pierce A <pbalworth@gmail.com>
2023-09-07 22:15:02 +0000

    turn down curl frequency to  100 from 200 ueransim_smoke_test.sh

--- ueransim_smoke_test.sh
+++ ueransim_smoke_test.sh
@@ -3,17 +3,10 @@ set -x
 
 apk add --no-cache jq
 # Test to run 200 curl commands to yahoo over 5G network and over the internet
-for i in {1..200}; do
+for i in {1..100}; do
     # Run the first curl command over the 5G network
     curl --interface uesimtun0 -w %{json} --url https://yahoo.com?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee -a over5g.json
 
     # Run the second curl command over the internet 
     curl --interface eth0 -w %{json} --url https://yahoo.com?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "overinternet"}' | tee -a overinternet.json
 done
-
-# Test to run a single curl command to yahoo over 5G network and over the internet
-# curl --interface uesimtun0 -w %{json} --url https://yahoo.com?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "over5g"}' | tee over5g.json
-
-# curl --interface eth0 -w %{json} --url https://yahoo.com?$RANDOM --silent --output /dev/null | jq -c '. + {"test": "overinternet"}' | tee overinternet.json
-
-# https://www.cia.gov/the-world-factbook/about/archives/download/factbook-2020.zip?$RANDOM
